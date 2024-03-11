53df4560cc9aee33f3afa55e93c322efc4a6c4b9
Mishgite <7594201@gmail.com>
2024-03-11 19:57:59 +0300

    страница

---
+++ data/users.py
@@ -0,0 +1,23 @@
+import datetime
+import sqlalchemy
+from flask_login import UserMixin
+from data.db_session import SqlAlchemyBase
+from sqlalchemy_serializer import SerializerMixin
+
+
+class User(SqlAlchemyBase, UserMixin, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    surname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String,
+                              index=True, unique=True, nullable=True)
+    password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    modified_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                      default=datetime.datetime.now)
+    address = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
+    def check_password(self, password: str) -> bool:
+        return password == self.hashed_password
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, url_for, request, render_template, redirect, abort
 import requests
 import json
@@ -12,18 +13,28 @@ from wtforms.validators import DataRequired, Email, NumberRange
 from flask_wtf import FlaskForm
 from flask_login import login_user, current_user, LoginManager, logout_user, login_required
 from data.universities import Universities
+from data.users import User
 
 app = Flask(__name__)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-db_session.global_init('db/universities.db')
+basedir = os.path.abspath(os.path.dirname(__file__))
+db_session.global_init(os.path.join(basedir, 'db', 'universities.db'))
 db_sess = db_session.create_session()
+
+
+@login_manager.user_loader
+def load_user(user_id: int):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 
 @app.route('/')
 def works_log():
     universities = db_sess.query(Universities).all()
-    return render_template('universities.html', title='Журнал работ', jobs=universities)
+    return render_template('universities.html', title='Журнал работ', universities=universities)
 
 
 if __name__ == '__main__':
